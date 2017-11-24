01e1d0575f1f7aec11cbc286d7b65dfe5b0cd635
Filipe Fernandes <filipefernandes007@gmail.com>
2017-11-24 11:05:43 +0000

    Add GET /todos/:id

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -4,6 +4,7 @@
 var {app}      = require('../server');
 var {Todo}     = require('../models/todo'); // the same as var Todo = require('./models/todo').Todo;
 var {SysError} = require('../models/sys-error');
+var {ObjectID} = require('mongodb');
 
 app.post('/todos', (req, res) => {
     try{
@@ -27,6 +28,29 @@ app.get('/todos', (req, res) => {
             res.status(200).send(result);
         }, (e) => {
             res.status(400).send(e);
+        });
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+app.get('/todos/:id', (req, res) => {
+    try {
+        var id = req.params.id;
+
+        if(!ObjectID.isValid(id)) {
+            return res.status(404).send(new SysError({text: 'Id not valid'}));
+        }
+
+        Todo.findById(id).then((todo) => {
+            if(!todo) {
+                return res.status(404)
+                          .send(new SysError({text: `Could not found todo with id=${id}`}));
+            }
+
+            res.status(200).send(todo);
+        }).catch((e) => {
+            return res.status(400).send(e.toString());
         });
     } catch (e) {
         res.status(400).send(e);
